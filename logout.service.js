7f0e2b880a3162acf499ddfebe25e182a07d0902
unknown <dsantos@daniels-pc.wpsnetwork.com>
2016-05-17 13:21:01 +0200

    Added error Location parameter.

--- logout.service.js
+++ logout.service.js
@@ -1,23 +1,23 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('onyxCommon').service('LogoutService', LogoutService);
+    angular.module('onyxCommon').service('LogoutService', LogoutService);
 
-  /** @ngInject */
-  function LogoutService($http, appConfig) { // jshint ignore:line
-    var service = {};
+    /** @ngInject */
+    function LogoutService($http, appConfig) { // jshint ignore:line
+        var service = {};
 
-    service.reset = function () {
-      //remove all persistent data
-      $http.defaults.headers.common['Authorization'] = '';// jshint ignore:line
-      appConfig.TOKEN = '';
-      localStorage.setItem('Authorization', '');
-    };
+        service.reset = function () {
+            //remove all persistent data
+            $http.defaults.headers.common['Authorization'] = '';// jshint ignore:line
+            appConfig.TOKEN = '';
+            localStorage.setItem('Authorization', '');
+        };
 
-    service.init = function () {
-      //this._initPropertyList();
-    };
+        service.init = function () {
+            //this._initPropertyList();
+        };
 
-    return service;
-  }
+        return service;
+    }
 })();
